705f61cf0558f9d9cb55068f2ec5e17dc389d682
agusmaulanayusufnoor <agusmaulanayusufnoor@gmail.com>
2023-04-11 23:55:00 +0700

    ecaleg

---
+++ ecaleg-adminone/src/router/index.js
@@ -0,0 +1,90 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Style from "@/views/StyleView.vue";
+import Home from "@/views/HomeView.vue";
+
+const routes = [
+  {
+    meta: {
+      title: "Masuk",
+    },
+    path: "/",
+    name: "style",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/LoginView.vue"),
+  },
+  {
+    // Document title tag
+    // We combine it with defaultDocumentTitle set in `src/main.js` on router.afterEach hook
+    meta: {
+      title: "Dashboard",
+    },
+    path: "/dashboard",
+    name: "dashboard",
+    component: Home,
+  },
+  {
+    meta: {
+      title: "Tables",
+    },
+    path: "/tables",
+    name: "tables",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/TablesView.vue"),
+  },
+  {
+    meta: {
+      title: "Forms",
+    },
+    path: "/forms",
+    name: "forms",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/FormsView.vue"),
+  },
+  {
+    meta: {
+      title: "Profile",
+    },
+    path: "/profile",
+    name: "profile",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/ProfileView.vue"),
+  },
+  {
+    meta: {
+      title: "Ui",
+    },
+    path: "/ui",
+    name: "ui",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/UiView.vue"),
+  },
+  {
+    meta: {
+      title: "Responsive layout",
+    },
+    path: "/responsive",
+    name: "responsive",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/ResponsiveView.vue"),
+  },
+  {
+    meta: {
+      title: "Login",
+    },
+    path: "/login",
+    name: "login",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/LoginView.vue"),
+  },
+  {
+    meta: {
+      title: "Error",
+    },
+    path: "/error",
+    name: "error",
+    component: () => import( /* webpackChunkName: "Auth" */ "@/views/ErrorView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition || { top: 0 };
+  },
+});
+
+export default router;
