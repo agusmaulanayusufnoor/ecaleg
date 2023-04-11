43986f02b7e7dae7ab6f769151d279870d076825
agusmaulanayusufnoor <agusmaulanayusufnoor@gmail.com>
2023-04-12 00:14:29 +0700

    conf-port

--- ecaleg-adminone/vite.config.js
+++ ecaleg-adminone/vite.config.js
@@ -7,6 +7,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   base: "/admin-one-vue-tailwind/",
   plugins: [vue()],
+  server:{
+    port : 3000
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
